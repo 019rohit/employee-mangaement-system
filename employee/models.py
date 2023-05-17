ce0bb68341e201b2f1f03780630f81865a5392b3
Rohit <rohitagarwal1922003@gmail.com>
2023-05-17 15:21:47 +0530

    registration page and sign up frontend and backend completed

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class EmployeeDetail(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    empcode = models.CharField(max_length=50)
+    empdept = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    joiningdate = models.DateField(null=True)
+    def __str__(self):
+        return self.user.username
+    
--- employee/views.py
+++ employee/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
@@ -6,7 +7,20 @@ def index(request):
     return render(request, 'index.html')
 
 def registration(request):
-    return render(request, 'registration.html')
+    error = ""
+    if request.method == "POST":
+        fn = request.POST['firstname']
+        ln = request.POST['lastname']
+        ec = request.POST['empcode']
+        em = request.POST['email']
+        pwd = request.POST['pwd']
+        try:
+            user = User.objects.create_user(first_name = fn, last_name = ln, username = em, password = pwd)
+            EmployeeDetail.objects.create(user = user, empcode = ec)
+            error = "no"
+        except:
+            error = "yes"
+    return render(request, 'registration.html', locals())
 
 def employeeLogin(request):
     return render(request, 'employeeLogin.html')
